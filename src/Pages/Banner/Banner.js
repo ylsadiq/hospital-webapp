004293e832694b166807abd29b63ddb84ced75c8
gm sadiq <gm1sadiq@gmail.com>
2022-09-22 21:17:42 +0600

    banner media query

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -7,13 +7,13 @@ const Banner = () => {
     return (
             <section className="hero-section mt-10">
                 <div className="container">
-                <div className="hero-container flex justify-items-center justify-between">
-  <div className='w-3/5 text-left'>
-      <h1 className="text-5xl font-bold"><span className='block'>Your <span className='text-sky-400'>Health</span> is Our</span>  Top <span className='text-blue-500'>Priority</span></h1>
+                <div className="hero-container md:flex justify-items-center justify-between">
+  <div className='md:w-3/5 md:text-left sm:text-center'>
+      <h1 className="lg:text-5xl sm:text-4xl font-bold"><span className='block'>Your <span className='text-sky-400'>Health</span> is Our</span>  Top <span className='text-blue-500'>Priority</span></h1>
       <p className="py-6 text-lg">There are many variations of passages of lpsum available, but the majority hae suffered.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
-        <div className='w-2/5'>
+        <div className='md:w-2/5'>
         <div class="outer_circle">
                         <div class="inner_circle">
                         <div className="inner-img">

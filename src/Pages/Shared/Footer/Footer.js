0f60adf7b93565b26472105ba8d6b05f18958733
gm sadiq <gm1sadiq@gmail.com>
2022-10-08 13:17:00 +0600

    contract us styled

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -15,17 +15,17 @@ const Footer = () => {
                         <p className='my-1.5'>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from React.</p>
                     </div>
                     <div className="emergency-contract grid grid-cols-2 gap-4">
-                        <div className="emergency">
+                        <div className="emergency-home">
                         <FontAwesomeIcon icon={faHouseMedical} />
-                            <div className="emergency-details">
+                            <div className="emergency-details emergency-home-details">
                             <h5>EMERGENCY CONTACT</h5>
                             <p>Free call 24/7</p>
                             <p>+1555 6761 020</p>
                             </div>
                         </div>
-                        <div className="home-visit">
+                        <div className="emergency-home">
                         <FontAwesomeIcon icon={faUserDoctor} />
-                        <div className="home-details">
+                        <div className="home-details emergency-home-details">
                             <h5>HOME VISIT</h5>
                             <p>Chargeable call 24/7</p>
                             <p>+1777 6761 050</p>
@@ -37,7 +37,15 @@ const Footer = () => {
                     <div className="social-heading grid grid-cols-2 gap-2">
                     <div className="tooth-whiting mt-3">
                         <div className="tooth-white">
+                        <div className="tooth-link">
                         <h2>Teeth <span className='block'>whitening</span></h2>
+                            <ul>
+                                <li><a className='bg-sky-400' href="">Laser treatment include</a></li>
+                                <li><a className='bg-sky-400' href="">Free teeth cleaning</a></li>
+                                <li><a className='bg-sky-400' href="">Additional 10% for next visit</a></li>
+                                <li><a className='bg-sky-400' href=""></a></li>
+                            </ul>
+                        </div>
                         <img src="https://i.ibb.co/s5vqHQ4/footer-img.png" alt="" />
                         </div>
                     </div>
